c12d28543e327080a4eb59ff4dcace15a0c15988
algoORgoal <qufstar46@gmail.com>
2019-08-23 00:07:24 +0900

    change

--- src/main/java/com/example/test_springboot/controller/RestAjaxController.java
+++ src/main/java/com/example/test_springboot/controller/RestAjaxController.java
@@ -1,20 +1,5 @@
 package com.example.test_springboot.controller;
 
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@CrossOrigin
-@RestController
-public class RestAjaxController {
-    @RequestMapping(value = "/api/ajaxll", method = RequestMethod.POST)
-    public String getParmWithAjax() {
-        return "Welcome to Hellow World !! ";
-    }
-}
-=======
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -34,4 +19,3 @@ public class RestAjaxController {
         return paramMap;
     }
 }
->>>>>>> 9def0392209c5b4cdeb88b7f6d81ebbba77e1612
